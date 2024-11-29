28719c791c834d9d0f3bd70069cc6094f3dd593b
Robin Hutmacher <robin.hutmacher@gmail.com>
2024-11-29 16:56:20 +0100

    Add button adding more lines

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
   import Pace from "./lib/Pace.svelte";
+
+  let paces = [1];
+
+  function addPace() {
+    paces = [...paces, paces.length + 1];
+  }
 </script>
 
 <main>
-  <Pace />
+  {#each paces as pace}
+    <Pace />
+  {/each}
+
+  <button on:click={addPace}>Add Pace</button>
 </main>
 
 <style>
